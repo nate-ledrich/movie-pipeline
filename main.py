578d216e979ea274940196bf57d79c75eba44d44
Nate <nathanael.ledrich@gmail.com>
2023-08-24 19:26:45 +0200

    feat: add data transformation

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from data_extraction import mysql_extractor
+from data_transformation import transformer
 
 
 def main():
-    # Extract data from MySQL
     data_by_table = mysql_extractor.extract_all_data()
 
     for table_name, data in data_by_table.items():
         print(f"Data extracted for table '{table_name}':", data)
+
+    transformed_data = transformer.transform_all_data(data_by_table)
+    print(transformed_data)
 
 
 if __name__ == "__main__":
