e9b8bd7bff3214cfe7871ae2a888d48a6a62ebc4
Nathanaël Ledrich <nathanael.ledrich@icloud.com>
2023-08-28 02:40:58 +0200

    refactor: turn transformer into a class

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from data_extraction.mysql_extractor import MySQLExtractor
 from data_loading.cassandra_loader import CassandraLoader
-from data_transformation import transformer
+from data_transformation.transformer import DataTransformer
 
 
 def main():
     extractor = MySQLExtractor()
     data_by_table = extractor.extract_all_data()
 
+    transformer = DataTransformer()
     transformed_data = transformer.transform_all_data(data_by_table)
 
     cassandra_loader = CassandraLoader()
